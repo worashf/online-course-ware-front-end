1af1b2d1849198d31968c1228987fd4ea70e2d2b
worashf <uppert83@gmail.com>
2022-11-15 11:18:38 +0300

    Update  video dtail styling

--- src/components/video/videoDetail.js
+++ src/components/video/videoDetail.js
@@ -14,6 +14,8 @@ const {Panel} = Collapse;
 const VideoDetail = () =>  {
     const {courseId,videoId} =useParams();
     const [course, setCourse] = useState();
+
+
     const findCourse = async() =>{
         const response = await axios.get(`http://localhost:8080/api/courses-id/${courseId}`);
         if(response.data.courseId){
@@ -33,23 +35,29 @@ return(
 <Row>
  <Col span={8}>
  <div className='course-material'>
- 
-      <Card title="video"  >
+     <div className='table-content'>
+       <h2 style={{textAlign:"center",fontWeight:"700",fontSize:"20px", marginTop:"10px"}}> Course Topics</h2>
+     {
+       course?.topics.map(topic =>(
+        
+
+      <Card title={topic.topicName}  headStyle ={{fontWeight:"700",fontSize:"20px"}} >
 
       <Collapse accordion>
-  
- <Panel header="" key="1">
-  
-     <Link to ="" className='video-link'> 
+     {topic.materials.map(material=>(
+ <Panel header={material.materialName} key={material.materialId}>
+   {material.videos.map(video=>(
+     <Link to ={`/video-detail/${courseId}/${video.videoId}`} className='video-link'> 
      <VideoCameraOutlined style={{color:"blue",marginRight:10}}/>
-     <h5> video </h5>
+     <h5> 
+     {video.videoTitle}</h5>
      </Link>
     
 
-   
+   ))}
  
 </Panel>
- 
+     ))}
    
      
 
@@ -58,12 +66,17 @@ return(
       </Card>
 
    
+       ))
+     }
+       </div>
+
+   
  </div>
  </Col>
  <Col span={16}>
  <div className='video' >
      <ReactPlayer url ={`http://localhost:8080/api/videos/video/${videoId}`}  
-     width="98%"  playing className="react-player" controls>
+     width="98%" height="60vh"  className="react-player" controls>
 
      </ReactPlayer>
  </div>
