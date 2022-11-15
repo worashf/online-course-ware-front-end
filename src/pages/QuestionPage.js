e2e7a7d8304af8b8fbfb65e6392ce91b62670963
worashf <uppert83@gmail.com>
2022-11-15 11:20:25 +0300

    Add question page

---
+++ src/pages/QuestionPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PageContainer from '../components/pageContaner';
+import Question from '../components/question/Question';
+
+const QuestionPage =() =>{
+
+    
+return (
+    <PageContainer>
+   <Question/>
+    </PageContainer>
+)
+
+}
+export default QuestionPage;
